4bf1ef23cf911714148f52d1e88eaf5a9a2b36fe
MostafaMagdySe <mostafamagdyse66@gmail.com>
2025-01-18 14:46:12 +0200

    Simplified adding email in application.properties

--- src/main/java/com/example/demo/Services/EmailSenderService.java
+++ src/main/java/com/example/demo/Services/EmailSenderService.java
@@ -1,18 +1,21 @@
 package com.example.demo.Services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailSenderService {
+    @Value("${email.service.address}")
+    private String your_email;
     private final JavaMailSender mailSender;
     public EmailSenderService (JavaMailSender mailSender){
         this.mailSender=mailSender;
     }
     public void SendEmail(String toEmail, String subject, String body) {
         SimpleMailMessage message= new SimpleMailMessage();
-        message.setFrom("your_email");
+        message.setFrom (your_email);
         message.setTo(toEmail);
         message.setText(body);
         message.setSubject(subject);
